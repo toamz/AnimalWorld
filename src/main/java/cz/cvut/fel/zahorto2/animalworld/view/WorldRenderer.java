5f7e435ff1966365d1c3c0820fa62a7d7ad0ceb3
Tomáš Záhorský <tomas.zahorsky@gmail.com>
2023-05-02 16:27:13 +0200

    WorldRenderer repaints every render frame

--- src/main/java/cz/cvut/fel/zahorto2/animalworld/view/WorldRenderer.java
+++ src/main/java/cz/cvut/fel/zahorto2/animalworld/view/WorldRenderer.java
@@ -5,6 +5,7 @@ import cz.cvut.fel.zahorto2.animalworld.model.World;
 import cz.cvut.fel.zahorto2.animalworld.model.entities.Entity;
 import cz.cvut.fel.zahorto2.animalworld.view.entities.EntityRenderer;
 import cz.cvut.fel.zahorto2.animalworld.view.tiles.TileRenderer;
+import javafx.animation.AnimationTimer;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
@@ -15,6 +16,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.transform.Affine;
 
 public class WorldRenderer extends ResizableCanvas implements EventHandler<Event> {
+    AnimationTimer repaintTimer;
+
     public WorldRenderer() {
         this.addEventHandler(ScrollEvent.SCROLL, this);
         this.addEventHandler(MouseEvent.MOUSE_MOVED, this);
@@ -22,6 +25,16 @@ public class WorldRenderer extends ResizableCanvas implements EventHandler<Event
         this.addEventHandler(MouseEvent.MOUSE_PRESSED, this);
         this.addEventHandler(MouseEvent.MOUSE_RELEASED, this);
         transform.appendScale(10, 10);
+
+        // Timer that repaints the canvas every frame
+        repaintTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                draw();
+                start();
+            }
+        };
+        repaintTimer.start();
     }
     Affine transform = new Affine();
     private World world;
@@ -63,10 +76,10 @@ public class WorldRenderer extends ResizableCanvas implements EventHandler<Event
             gc.save();
             for (int y = 0; y < world.getHeight(); y++) {
                 TileRenderer.render(world.getTileGrid().getTile(x, y), gc);
-                gc.translate(1, 0);
+                gc.translate(0, 1);
             }
             gc.restore();
-            gc.translate(0, 1);
+            gc.translate(1, 0);
         }
         gc.restore();
     }
@@ -91,7 +104,6 @@ public class WorldRenderer extends ResizableCanvas implements EventHandler<Event
         transform.appendTranslation(mousePos.x, mousePos.y);
         transform.appendScale(zoomFactor, zoomFactor);
         transform.appendTranslation(-mousePos.x, -mousePos.y);
-        draw();
         System.out.println("Zoomed " + zoomFactor);
     }
 
@@ -113,7 +125,6 @@ public class WorldRenderer extends ResizableCanvas implements EventHandler<Event
                 CoordDouble delta = CoordDouble.subtract(mousePos, dragLast);
                 dragLast = mousePos;
                 transform.appendTranslation(delta.x, delta.y);
-                draw();
             }
             CoordDouble mousePos = new CoordDouble(mouseEvent.getX(), mouseEvent.getY());
             CoordDouble mapPos = viewToMapCoord(mousePos);
@@ -123,6 +134,5 @@ public class WorldRenderer extends ResizableCanvas implements EventHandler<Event
 
     public void setWorld(World world) {
         this.world = world;
-        draw();
     }
 }
