16960085f163e15372816fc0eb39f3c3de926ac1
Tomáš Záhorský <tomas.zahorsky@gmail.com>
2023-05-23 09:32:10 +0200

    Statistics for grown grass

--- src/main/java/cz/cvut/fel/zahorto2/animalworld/view/StatisticsLabel.java
+++ src/main/java/cz/cvut/fel/zahorto2/animalworld/view/StatisticsLabel.java
@@ -2,6 +2,7 @@ package cz.cvut.fel.zahorto2.animalworld.view;
 
 import cz.cvut.fel.zahorto2.animalworld.model.World;
 import cz.cvut.fel.zahorto2.animalworld.model.entities.EntityType;
+import cz.cvut.fel.zahorto2.animalworld.model.tiles.Grass;
 import javafx.animation.AnimationTimer;
 import javafx.scene.control.Label;
 
@@ -36,11 +37,23 @@ public class StatisticsLabel extends Label implements World.TickListener {
      * Get the number of entities of a given type
      * @param entityType Type of the entity to count
      */
-    public int getEntityCount(EntityType entityType) {
+    private int getEntityCount(EntityType entityType) {
         int count = 0;
         for (var entity : world.getEntityMap().getEntities()) {
             if (entity.getType() == entityType)
                 count++;
+        }
+        return count;
+    }
+
+    private int getEatableGrassCount() {
+        int count = 0;
+        for (int x = 0; x < world.getTileGrid().getWidth(); x++) {
+            for (int y = 0; y < world.getTileGrid().getHeight(); y++) {
+                if (world.getTileGrid().getTile(x, y) instanceof Grass grass && grass.isEatable())
+                        {count++;
+                }
+            }
         }
         return count;
     }
@@ -54,6 +67,8 @@ public class StatisticsLabel extends Label implements World.TickListener {
         statistics.append("Entities: ").append(world.getEntityMap().getEntities().length).append("\n");
         statistics.append("Sheep: ").append(getEntityCount(EntityType.SHEEP)).append("\n");
         statistics.append("Wolf: ").append(getEntityCount(EntityType.WOLF)).append("\n");
+        statistics.append("\n");
+        statistics.append("Eatable grass: ").append(getEatableGrassCount()).append("\n");
 
         setText(statistics.toString());
     }
