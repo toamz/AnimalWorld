cdd8d991c2f961929efc021881b2a87b757f1055
Tomáš Záhorský <tomas.zahorsky@gmail.com>
2023-05-24 14:34:44 +0200

    remove redundant Main class + set jar mainClass

--- src/main/java/cz/cvut/fel/zahorto2/animalworld/Main.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fel.zahorto2.animalworld;
-
-import cz.cvut.fel.zahorto2.animalworld.controller.App;
-
-public class Main {
-    public static void main(String[] args) {
-        App.main(args);
-    }
-}
